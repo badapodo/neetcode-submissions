e7ae1c1e1e6ebc223631a21884b2e80c1a22f8c6
김성광 <128721477+badapodo@users.noreply.github.com>
2026-09-08 21:19:00 +0900

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        if (s.charAt(0) != '0') {
+            dp[1] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            int one = s.charAt(i - 1) - '0';
+            int two = one + (s.charAt(i - 2) - '0') * 10;
+
+            if (one != 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
